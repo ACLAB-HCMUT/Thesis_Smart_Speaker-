64409862337dccb77cdfe12f9fe26369fc96957a
Duong Nguyen <144302817+NguyenDuong163@users.noreply.github.com>
2024-10-29 13:40:29 +0700

    Small update for main.cpp

--- ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/main.cpp
+++ ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/main.cpp
@@ -8,22 +8,20 @@
 #include "freertos/task.h"
 #include "tasks/led_Task.h"  // Include file task led
 #include "tasks/mqtt_Task.h"
-
 // Replace with your network credentials
 // const char* ssid = PROJECT_WIFI_SSID;
 // const char* password = PROJECT_WIFI_PASSWORD;
 
-const char* ssid = "DCrab";
-const char* password = "zzzzzzzz";
+const char* ssid = "ACLAB";
+const char* password = "ACLAB2023";
 
 
 // Set LED GPIO
-const int ledPin = 45;
+const int ledPin = 21;
 // Stores LED state
 
 // Task to handle Wi-Fi connection
 void wifiTask(void *pvParameters) {
-  Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -35,23 +33,14 @@ void wifiTask(void *pvParameters) {
   vTaskDelete(NULL);  // Delete the task when done
 }
 
-
 void setup(){
-  // pinMode(ledPin, OUTPUT);
-  // pixels.begin();
-  // // Create tasks for Wi-Fi and server
-  // xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
-  // createLedTask(ledPin);
   Serial.begin(115200);  // Initialize Serial communication
-
-  // Initialize the LED control
-  //createLedTask(45);
-
-  // Create tasks for Wi-Fi and MQTT
   xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
+  // Create tasks for Wi-Fi and MQTT
+  
   createMqttTask();  // Initialize MQTT task
+  //createSingleLedTask();
 }
- 
 void loop(){
   // Nothing to do here, FreeRTOS tasks handle the work
 }
