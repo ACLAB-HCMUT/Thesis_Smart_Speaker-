2be74c5ac0fc4c7ae1767d8f6927f086852cfad4
locphan <loc.phan2113971@hcmut.edu.vn>
2024-11-25 11:56:12 +0700

    update: import in process

--- command/my_calendar.py
+++ command/my_calendar.py
@@ -1,12 +1,13 @@
 import datetime
 import os.path
-from listen import *
-from speak import *
+from listen import listen_command
+from speak import speak
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import re
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
