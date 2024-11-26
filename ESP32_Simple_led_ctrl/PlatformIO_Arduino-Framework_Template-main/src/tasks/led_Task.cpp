0a116038d3816262145cd3e9b135a3ab579e6a25
Duong Nguyen <144302817+NguyenDuong163@users.noreply.github.com>
2024-11-26 15:38:58 +0700

    Update led_Task

--- ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/tasks/led_Task.cpp
+++ ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/tasks/led_Task.cpp
@@ -1,13 +1,24 @@
 // led_Task.cpp
 #include "led_Task.h"
 
-Adafruit_NeoPixel pixels(4, 8, NEO_GRB + NEO_KHZ800);
-// Task để xử lý input từ Serial để điều khiển LED
-void led_handle(int ledIndex, const char* message) {
+Adafruit_NeoPixel pixels1(4, 8 , NEO_GRB + NEO_KHZ800); // D5-D6
+Adafruit_NeoPixel pixels2(4, 10, NEO_GRB + NEO_KHZ800); // D7-D8
+
+
+void led_handle1(int ledIndex, const char* message) {
   if (strcmp(message, "ON") == 0) {
-    pixels.setPixelColor(ledIndex, pixels.Color(255, 255, 255));
+    pixels1.setPixelColor(ledIndex, pixels1.Color(255, 255, 255));
   } else if (strcmp(message, "OFF") == 0) {
-    pixels.setPixelColor(ledIndex, pixels.Color(0, 0, 0));
+    pixels1.setPixelColor(ledIndex, pixels1.Color(0, 0, 0));
   }
-  pixels.show();
+  pixels1.show();
+}
+
+void led_handle2(int ledIndex, const char* message) {
+  if (strcmp(message, "ON") == 0) {
+    pixels2.setPixelColor(ledIndex, pixels2.Color(255, 255, 255));
+  } else if (strcmp(message, "OFF") == 0) {
+    pixels2.setPixelColor(ledIndex, pixels2.Color(0, 0, 0));
+  }
+  pixels2.show();
 }
