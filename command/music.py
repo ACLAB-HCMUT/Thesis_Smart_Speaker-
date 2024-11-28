129d70775781c567f56d65f6eb2b952144ea1b9c
locphan <loc.phan2113971@hcmut.edu.vn>
2024-11-28 21:58:50 +0700

    add stop music

--- command/music.py
+++ command/music.py
@@ -3,7 +3,8 @@ from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
 import subprocess
-from speak import speak
+import threading
+from listen import listen_command
 load_dotenv()
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
@@ -21,7 +22,6 @@ def search_youtube(query):
             video_id = response["items"][0]["id"]["videoId"]
             video_title = response["items"][0]["snippet"]["title"]
             print(f"Tìm thấy video: {video_title}")
-            speak(f"Mời bạn nghe nhạc {video_title}.")
             return f"https://www.youtube.com/watch?v={video_id}"
         else:
             print("Không tìm thấy kết quả.")
@@ -29,37 +29,6 @@ def search_youtube(query):
     except Exception as e:
         print("Lỗi khi tìm kiếm YouTube:", e)
         return None
-
-music_process = None
-audio_temp_file = "audio.webm"
-def delete_old_audio_file():
-    if os.path.exists(audio_temp_file):
-        os.remove(audio_temp_file)
-        print(f"Đã xóa file nhạc cũ: {audio_temp_file}")
-def download_and_play_youtube_audio(video_url):
-    global music_process
-    try:
-        delete_old_audio_file()
-        options = {
-            'format': 'bestaudio/best',  
-            'outtmpl': 'audio.%(ext)s', 
-            'noplaylist': True,          
-        }
-        
-        with YoutubeDL(options) as ydl:
-            info = ydl.extract_info(video_url, download=True)
-            audio_file = ydl.prepare_filename(info)
-        
-        music_process = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", audio_file])
-
-        music_process.wait()
-
-     
-        os.remove(audio_file)
-        print("Hoàn tất!")
-    except Exception as e:
-        print("Đã xảy ra lỗi:", e)
-        music_process = None
 
 def stop_music():
     global music_process
@@ -69,3 +38,51 @@ def stop_music():
         music_process = None
     else:
         print("Không có nhạc đang phát.")
+def listen_for_stop_command():
+    while True:
+        command = listen_command()  
+        if command and ("dừng nhạc" in command or "tắt nhạc" in command or "tắt" in command):
+            stop_music()  
+            break
+music_process = None
+
+def delete_old_audio_file():
+    audio_temp_file = "audio.webm"
+    if os.path.exists(audio_temp_file):
+        os.remove(audio_temp_file)
+        print(f"Đã xóa file nhạc cũ: {audio_temp_file}")
+    audio_part_file = f"audio.webm.part"
+    if os.path.exists(audio_part_file):
+        os.remove(audio_part_file)
+        print(f"Đã xóa file tạm thời: {audio_part_file}")
+
+def download_and_play_youtube_audio(video_url):
+    global music_process
+    try:
+        delete_old_audio_file()
+        options = {
+            'format': 'bestaudio/best',  
+            'outtmpl': 'audio.%(ext)s', 
+            'noplaylist': True,
+            'no_part': True,
+            'audioquality': 1,
+
+                                     
+        }
+        
+        with YoutubeDL(options) as ydl:
+            info = ydl.extract_info(video_url, download=True)
+            audio_file = ydl.prepare_filename(info)
+        
+        music_process = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", audio_file])
+        stop_thread = threading.Thread(target=listen_for_stop_command)
+        stop_thread.start()
+
+        music_process.wait()
+
+     
+        os.remove(audio_file)
+        print("Hoàn tất!")
+    except Exception as e:
+        print("Đã xảy ra lỗi:", e)
+        music_process = None
