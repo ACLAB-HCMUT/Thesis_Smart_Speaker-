ae58e21684f3090c1f5773ad7453ad14b28ad8e1
Duong Nguyen <144302817+NguyenDuong163@users.noreply.github.com>
2024-12-02 16:50:48 +0700

    Update mqtt_tx_Task

--- ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/tasks/mqtt_tx_Task.h
+++ ESP32_Simple_led_ctrl/PlatformIO_Arduino-Framework_Template-main/src/tasks/mqtt_tx_Task.h
@@ -2,7 +2,10 @@
 #define MQTT_TX_TASK_H
 
 #include <PubSubClient.h>
-
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
 void createMQTT_TX();
 void mqtt_txTask(void *pvParameters);
 
