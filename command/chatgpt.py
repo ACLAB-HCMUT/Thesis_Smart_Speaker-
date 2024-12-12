0cd821115ef77e7d99d9ca8e09ad4bf4d950241a
locphan <loc.phan2113971@hcmut.edu.vn>
2024-12-12 12:28:02 +0700

    update chatgpt

--- command/chatgpt.py
+++ command/chatgpt.py
@@ -6,7 +6,11 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 CHATGPT_API_URL = os.getenv('CHATGPT_API_URL')
 
+conversation_history = []
+
 def chatgpt_response(prompt):
+    global conversation_history
+    conversation_history.append({"role": "user", "content": prompt})
     headers = {
         "Content-Type":"application/json",
         "Authorization":f"Bearer {API_KEY}"  
@@ -16,8 +20,8 @@ def chatgpt_response(prompt):
         "model": "gpt-3.5-turbo",
         "messages": [
             {"role": "system", "content": "Bạn là một trợ lý điều khiển nhà thông minh tên là Aya! Bạn cũng là một trợ lý ảo thông minh giống như Alexa.Người dùng mà bạn đang hỗ trợ là chủ sở hữu của hệ thống này, hãy luôn nhớ rằng đây là người bạn phục vụ."},
-            {"role": "user", "content": prompt}
-        ],
+            # {"role": "user", "content": prompt}
+        ]+ conversation_history,
         "max_tokens": 100,
         "stream": False,
         "temperature": 0.5,
@@ -29,7 +33,11 @@ def chatgpt_response(prompt):
         response.raise_for_status()
         result = response.json()
     
-        return result['choices'][0]['message']['content']
+        # return result['choices'][0]['message']['content']
+        chatgpt_reply = result['choices'][0]['message']['content']
+        conversation_history.append({"role": "assistant", "content": chatgpt_reply})
+        return chatgpt_reply
+
     except requests.exceptions.RequestException as e:
         print(f"-----------------> Error calling ChatGPT API: {e}")
         return "Xin lỗi, không thể kết nối với dịch vụ API."
--- command/process.py
+++ command/process.py
@@ -142,63 +142,6 @@ def process_command(command):
     elif "kể" in command and ("truyện" in command or "chuyện" in command):
         print("Đang kể truyện...")
         play_story_sound()
-    elif is_device_command(command):
-        actions = {"bật": "on", "mở": "on", "tắt": "off", "đóng": "off"}
-        devices = {"đèn": "led1", "quạt": "fan", "cửa": "door", "máy lạnh": "ac"}
-        rooms = ["phòng khách", "phòng ngủ", "phòng bếp", "phòng làm việc"]
-
-        room_pattern = r"\b(" + "|".join(rooms) + r")\b"
-        device_pattern = r"\b(" + "|".join(devices) + r")\b"
-        action_pattern = r"\b(" + "|".join(actions.keys()) + r")\b"
-
-        room_match = re.search(room_pattern, command, re.IGNORECASE)
-        device_match = re.search(device_pattern, command, re.IGNORECASE)
-        action_match = re.search(action_pattern, command, re.IGNORECASE)
-        response = ""
-
-        # case 1: full command
-        if room_match and device_match and action_match:
-            room = room_match.group(0)
-            device = device_match.group(0)
-            action = actions[action_match.group(0).lower()]
-            response = f"Đã {action_match.group(0).lower()} {device} ở {room}"
-            feed_name = devices[device]
-            control_device(action, feed_name)
-
-        # case 2: missing device, but room, action are present
-        elif room_match and action_match and not device_match:
-            room = room_match.group(0)
-            action = actions[action_match.group(0).lower()]
-            response = f"Vui lòng chỉ định thiết bị để {action_match.group(0).lower()} ở {room}."
-
-        # case 3: missing action, but room, device are present
-        elif room_match and device_match and not action_match:
-            room = room_match.group(0)
-            device = device_match.group(0)
-            response = f"Vui lòng chỉ định hành động cho {device} ở {room}."
-
-        # case 4: room mentioned but missing both action and device
-        elif room_match and not action_match and not device_match:
-            room = room_match.group(0)
-            response = f"Vui lòng chỉ định thiết bị và hành động ở {room}."
-        elif action_match and device_match and not room_match:
-            device = device_match.group(0)
-            action = action_match.group(0)
-            response = (
-                f"Vui lòng chỉ định phòng để {action_match.group(0).lower()} {device}."
-            )
-        elif action_match and device_match and not room_match:
-            device = device_match.group(0)
-            action = action_match.group(0)
-            response = (
-                f"Vui lòng chỉ định phòng để {action_match.group(0).lower()} {device}."
-            )
-        # case 5: command not recognized
-        else:
-            response = "Lệnh không được nhận diện, vui lòng thử lại."
-
-        print(response)
-        speak(response)
     elif any(keyword in command for keyword in ["đường từ", "tìm đường", "chỉ đường", "hướng dẫn đường", "đường đi từ", "hỏi đường"]):
         process_direction(command)
     elif any(
