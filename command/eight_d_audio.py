1a370e6829af847209e0f55946c0df450c8e4d6e
locphan <loc.phan2113971@hcmut.edu.vn>
2024-12-13 18:17:38 +0700

    update

--- command/eight_d_audio.py
+++ command/eight_d_audio.py
@@ -1,4 +1,3 @@
-# eight_d_audio.py
 import pygame
 import math
 import time
@@ -13,11 +12,11 @@ class EightDAudio:
             pygame.mixer.set_num_channels(pygame.mixer.get_num_channels() + 1)
             self.channel = pygame.mixer.Channel(pygame.mixer.get_num_channels() - 1)
         self.channel.play(self.sound, loops=loops, fade_ms=fade_ms)
-        self.channel.set_volume(0.5, 0.5)  # Bắt đầu với âm lượng trung bình cho cả hai kênh
+        self.channel.set_volume(0.5, 0.5)  
 
-        self.stride = stride  # Bước tăng giảm góc
-        self.speed = speed    # Tốc độ cập nhật âm lượng
-        self.angle = 0        # Góc hiện tại để tính toán âm lượng
+        self.stride = stride  
+        self.speed = speed    
+        self.angle = 0       
 
         self.running = True
         self.thread = threading.Thread(target=self.update_volume)
@@ -25,12 +24,10 @@ class EightDAudio:
 
     def update_volume(self):
         while self.running:
-            # Tính toán âm lượng dựa trên góc hiện tại
             left = 0.5 * (1 + math.sin(math.radians(self.angle)))
             right = 0.5 * (1 + math.cos(math.radians(self.angle)))
             self.channel.set_volume(left, right)
 
-            # Cập nhật góc
             self.angle = (self.angle + self.stride) % 360
 
             time.sleep(self.speed)
